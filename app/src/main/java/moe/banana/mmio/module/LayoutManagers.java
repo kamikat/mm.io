ec10c4d82a75377c3783ecadf3340bbda81aa6fe
kamikat <kirisetsz@gmail.com>
2016-09-07 22:15:03 +0800

    Provide LayoutManagers

--- app/src/main/java/moe/banana/mmio/module/LayoutManagers.java
+++ app/src/main/java/moe/banana/mmio/module/LayoutManagers.java
@@ -1,8 +1,10 @@
 package moe.banana.mmio.module;
 
 import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -20,23 +22,45 @@ public class LayoutManagers {
     @Qualifier
     @Documented
     @Retention(RetentionPolicy.RUNTIME)
-    public @interface Linear { }
+    public @interface Linear {
+        int orientation() default LinearLayoutManager.VERTICAL;
+    }
 
     @Qualifier
     @Documented
     @Retention(RetentionPolicy.RUNTIME)
-    public @interface Grid2 { }
+    public @interface Grid {
+        int columns() default 2;
+        int orientation() default GridLayoutManager.VERTICAL;
+    }
 
     @Qualifier
     @Documented
     @Retention(RetentionPolicy.RUNTIME)
-    public @interface Staggered { }
+    public @interface Staggered {
+        int columns() default 2;
+        int orientation() default StaggeredGridLayoutManager.VERTICAL;
+    }
 
     @Provides
     @ActivityScope
     @Linear
     public static RecyclerView.LayoutManager provideLinearLayoutManager(Context context) {
         return new LinearLayoutManager(context);
+    }
+
+    @Provides
+    @ActivityScope
+    @LayoutManagers.Grid
+    public static RecyclerView.LayoutManager provideGridLayoutManager(Context context) {
+        return new GridLayoutManager(context, 2);
+    }
+
+    @Provides
+    @ActivityScope
+    @LayoutManagers.Staggered
+    public static RecyclerView.LayoutManager provideStaggeredLayoutManager(Context context) {
+        return new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
     }
 
 }

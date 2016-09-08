8c6cbb18938dfd19499fb6c192acf1dfadf1e4a9
kamikat <kirisetsz@gmail.com>
2016-09-08 18:22:11 +0800

    Simplify LayoutManagers

--- app/src/main/java/moe/banana/mmio/module/LayoutManagers.java
+++ app/src/main/java/moe/banana/mmio/module/LayoutManagers.java
@@ -3,8 +3,6 @@ package moe.banana.mmio.module;
 import android.content.Context;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -22,45 +20,21 @@ public class LayoutManagers {
     @Qualifier
     @Documented
     @Retention(RetentionPolicy.RUNTIME)
-    public @interface Linear {
-        int orientation() default LinearLayoutManager.VERTICAL;
-    }
-
-    @Qualifier
-    @Documented
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface Grid {
-        int columns() default 2;
-        int orientation() default GridLayoutManager.VERTICAL;
-    }
-
-    @Qualifier
-    @Documented
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface Staggered {
-        int columns() default 2;
-        int orientation() default StaggeredGridLayoutManager.VERTICAL;
+    public @interface Columns {
+        int value();
     }
 
     @Provides
     @ActivityScope
-    @Linear
-    public static RecyclerView.LayoutManager provideLinearLayoutManager(Context context) {
+    public static LinearLayoutManager provideLinearLayoutManager(Context context) {
         return new LinearLayoutManager(context);
     }
 
     @Provides
     @ActivityScope
-    @LayoutManagers.Grid
-    public static RecyclerView.LayoutManager provideGridLayoutManager(Context context) {
+    @Columns(2)
+    public static GridLayoutManager provideGridLayoutManager(Context context) {
         return new GridLayoutManager(context, 2);
-    }
-
-    @Provides
-    @ActivityScope
-    @LayoutManagers.Staggered
-    public static RecyclerView.LayoutManager provideStaggeredLayoutManager(Context context) {
-        return new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
     }
 
 }
--- app/src/main/java/moe/banana/mmio/presenter/MainPresenter.java
+++ app/src/main/java/moe/banana/mmio/presenter/MainPresenter.java
@@ -1,12 +1,12 @@
 package moe.banana.mmio.presenter;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import javax.inject.Inject;
 
 import moe.banana.mmio.scope.ActivityScope;
-import moe.banana.mmio.module.LayoutManagers;
 import moe.banana.mmio.service.Gank;
 import moe.banana.mmio.view.MainViewModel;
 
@@ -16,7 +16,7 @@ public class MainPresenter extends ActivityPresenter {
     @Inject public Gank api;
     @Inject public MainViewModel vm;
     @Inject public RecyclerView.Adapter<?> adapter;
-    @Inject public @LayoutManagers.Linear RecyclerView.LayoutManager layoutManager;
+    @Inject public LinearLayoutManager layoutManager;
 
     @Inject MainPresenter() { }
 

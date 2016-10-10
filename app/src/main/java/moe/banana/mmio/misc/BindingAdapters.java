0bab27b245723a8cc863575cbd88e61f4db85da7
kamikat <kirisetsz@gmail.com>
2016-10-10 15:23:49 +0800

    Fix binding optimization

--- app/src/main/java/moe/banana/mmio/misc/BindingAdapters.java
+++ app/src/main/java/moe/banana/mmio/misc/BindingAdapters.java
@@ -1,6 +1,7 @@
 package moe.banana.mmio.misc;
 
 import android.databinding.BindingAdapter;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -10,5 +11,10 @@ public final class BindingAdapters {
     @BindingAdapter("src")
     public static void bindImage(ImageView view, String url) {
         Picasso.with(view.getContext()).load(url).fit().centerCrop().into(view);
+    }
+
+    @BindingAdapter("itemFactory")
+    public static void bindItemFactory(RecyclerView view, ItemViewFactory factory) {
+        // do nothing
     }
 }

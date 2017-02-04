07c4f25d1369ddc97ee0a13c368d67704e6ccfe0
kamikat <kamikat@banana.moe>
2017-02-04 18:26:58 +0800

    add comments on ArticlePresenterFragment

--- app/src/main/java/moe/banana/mmio/ArticlePresenterFragment.java
+++ app/src/main/java/moe/banana/mmio/ArticlePresenterFragment.java
@@ -3,6 +3,12 @@ package moe.banana.mmio;
 import dagger.Module;
 import moe.banana.mmio.view.ArticleViewModel;
 
+/**
+ * Helper class representing any fragment capable with {@link ArticlePresenter}
+ *
+ * Workaround the problem of a generic typed class cannot be used as a valid dependency
+ * in {@link dagger.Component} declaration
+ */
 @Module
 public class ArticlePresenterFragment extends PresenterFragment<ArticleViewModel, ArticlePresenter> {
 
